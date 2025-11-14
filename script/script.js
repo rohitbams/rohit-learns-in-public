dd396d5b5cd685546881f7bf368f25c0e65daa9b
rb341 <rb341@st-andrews.ac.uk>
2025-11-14 23:15:58 +0000

    add js, start frist blog post rohit learns c

---
+++ script/script.js
@@ -0,0 +1,30 @@
+const loadPosts = async () => {
+    const postContainer = document.querySelector(".post");
+
+    try {
+        const response = await fetch("posts.json");
+        const posts = await response.json();
+
+        let html = "";
+
+        for (let post of posts) {
+            // Fetch the post's HTML file
+            const postResponse = await fetch(post.file);
+            const postHTML = await postResponse.text();
+
+            html += `
+                <div class="post-preview">
+                    <h2 id="post-title">${post.title}</h2>
+                    <span class="date">${post.date} - ${post.time}</span>
+                    <div class="post-body">${postHTML}</div>
+                </div>
+            `;
+        }
+
+        postContainer.innerHTML = html;
+    } catch (err) {
+        console.error("Error:", err);
+    }
+};
+
+loadPosts();
